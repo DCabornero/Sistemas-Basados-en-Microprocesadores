0595c549663b7d517b07f902b85c4acb49886fac
sergamar <sergamar23@hotmail.es>
2019-05-01 15:44:41 +0200

    Listo para entregar

--- p4/p4a.asm
+++ p4/p4a.asm
@@ -33,12 +33,12 @@ nodes DB 'El driver no esta instalado o no es este driver.$'
 contador DW 0
 index DW 0
 finished DB 0
-empty DB '$'
-
-;;Macros
+off1C DW (?)
+seg1C DW (?)
 firma DW 0DCABH
+;;Macros
 codnum equ 10h
-decodnum equ 10h
+decodnum equ 11h
 ascii equ 31h
 tabledim equ 6
 endstring equ '$'
@@ -47,6 +47,7 @@ args equ 80H
 
 ; Rutina de servicio a la interrupción
 rsi PROC FAR
+	sti 	;Habilitamos las interrupciones para que 1Ch se pueda ejecutar
 	; Salva registros modificados
 	push ax bx cx dx bp ds di si
 	; Instrucciones de la rutina ...
@@ -55,15 +56,14 @@ rsi PROC FAR
 	cmp ah, decodnum ;Si la interrupcion es 11h, decodificamos
 	jz decod
 	jmp fin	; Si no es ninguno de los dos casos no hacemos nada
-	cod: ;Codificamos una String y la imprimimos
+cod: ;Codificamos una String y la imprimimos
 	mov bp, dx
 	mov si, 0
 	mov di, 0
 	mov bl, ds:[bp][si]	;Obtenemos el primer digito sin codificar
-	mov bl, ds:[bp][si]	;Obtenemos el primer digito sin codificar
-	loopcod:
+loopcod:
 	mov bh, 0
-	sal bx, 1
+	sal bx, 1						;En tablacod cada "información" ocupa dos bytes
 	mov bx, WORD PTR tablacod[bx]	;Obtenemos el primer digito codificado
 	mov WORD PTR salida[di], bx		;Guardamos la letra en la cadena codificada
 	inc si							;Avanzamos posicion en la String
@@ -73,12 +73,12 @@ rsi PROC FAR
 	jnz loopcod						;Repetimos el proceso si no es asi
 	mov salida[di], '$'				;Finalizamos la cadena codificada
 	jmp print						;Saltamos a la impresion por pantalla
-	decod:	;Decodificamos una String y la imprimimos
+decod:	;Decodificamos una String y la imprimimos
 	mov bp, dx
 	mov cx, 0
 	mov di, 0
 	mov bh, 0
-	loopdecod:
+loopdecod:
 	mov bl, ds:[bp][di]+1			;Obtenemos la direccion de la letra decodificada con mayor peso
 	sub bl, ascii					;Cambiamos la letra de formato ASCII al digito en si
 	mov si, bx						
@@ -103,15 +103,10 @@ rsi PROC FAR
 	;mov ah, 9
 	;int 21h
 	mov finished,1					;Cambiamos la flag para indicar que la decodificacion ha acabado
-	check:							;Comprobacion de si se ha cambiado la flag de finalizacion
-	mov ax, cs						;Impresion vacia para evitar optimización del compilador
-    mov ds, ax						
-    mov dx, OFFSET empty
-    mov ah, 9
-    int 21H
+check:							;Comprobacion de si se ha cambiado la flag de finalizacion
 	cmp finished,0					
 	jnz check	
-	fin:
+fin:
 	; Recupera registros modificados
 	pop si di ds bp dx cx bx ax
 	iret
@@ -133,127 +128,23 @@ rsi2 PROC FAR
 	mov ah, 2
 	int 21h
 	inc index				;Incrementamos el contador de letra contada
-	reset:					;Reiniciamos el contador
+reset:					;Reiniciamos el contador
 	mov contador, 0			
 	jmp retorno
 	terminate:				;Hemos terminado, ponemos todas las flags y contadores a cero
 	mov finished, 0
 	mov index, 0
 	mov contador, 0
-	retorno:				
+retorno:				
 	; Recupera registros modificados
 	pop ds ax dx bx
 	iret
 
 rsi2 ENDP
 
-instalador PROC FAR
-	xor dh, dh
-	mov dl, cs:[args] ; Número de argumentos
-	cmp dl, 0
-	jz aux ; Si no hay argumentos, mostramos unas instrucciones, pues no es una opcion valida
-	cmp dl, 3
-	jz instdes	;Si hay tres letras, PUEDE que haya puesto correctamente las letras
-	aux:
-	jmp instrucciones	;Si nos pasan algo no contemplado, imprimimos instrucciones
-	instdes:		;Comprobamos si las tres letras son validas
-	mov dl, cs:[args+3]
-	cmp dl, 'D'		;Si la tercera letra es D, desinstalamos
-	jz desinst
-	cmp dl, 'I'		;Si la tercera letra es I, instalamos
-	jnz aux
-	;inst:
-	mov cx, 0		
-	mov es, cx
-	mov cx, OFFSET rsi	;ax tiene el OFFSET del codigo Polibio
-	mov bx, cs
-	;cli					;Se inhabilitan interrupciones
-	in ax, 21h
-	or ax, 1
-	out 21h, ax
-	
-	mov es:[57h*4], cx		;Implementamos una rutina de servicio a la interrupcion 57h
-	mov es:[57h*4+2], bx
-	
-	in ax, 21h
-	and ax, 1111111111111110b
-	out 21h, ax
-	;sti					;Las interrupciones vuelven a estar habilitadas
-	;mov dx, OFFSET instalador
-	
-	mov cx, 0		
-	mov es, cx
-	mov cx, OFFSET rsi2	;ax tiene el OFFSET del codigo Polibio
-	mov bx, cs
-	;cli					;Se inhabilitan interrupciones
-	
-	in ax, 21h
-	or ax, 1
-	out 21h, ax
-	
-	mov es:[1Ch*4], cx		;Implementamos una rutina de servicio a la interrupcion 1Ch
-	mov es:[1Ch*4+2], bx
-	
-	in ax, 21h
-	and ax, 1111111111111110b
-	out 21h, ax
-	;sti					;Las interrupciones vuelven a estar habilitadas
-	mov dx, OFFSET instalador
-	int 27h ; Acaba y deja residente 
-			; PSP, variables y rutina rsi.
-	
-	desinst:
-	;Comprobamos si el driver que está instalado es el nuestro, si es que hay alguno
-	mov ax, 0				
-	mov es, ax				
-	cmp ax, es:[57h*4]	;Comprobamos si hay un driver instalado	
-	jz nodesinst		
-	mov ax, firma		;Firma de nuestro driver
-	mov si, es:[57h*4]
-	mov es, es:[57h*4+2]
-	cmp ax, es:[si-2]	;Comprobamos si la firma de nuestro driver es la que debe ser
-	jnz nodesinst		
-	mov cx, 0				;Desinstalacion primer driver
-	mov ds, cx
-	mov es, ds:[57h*4+2]
-	mov bx, es:[2Ch]
-	
-	mov ah, 49h
-	int 21h
-	mov es, bx
-	int 21h
-	
-	cli ;TODO
-	mov ds:[57h*4], cx
-	mov ds:[57h*4+2], cx
-	sti	;TODO
-	
-	;mov cx, 0
-	;mov ds, cx
-	;mov es, ds:[1Ch*4+2]	;Desisntalacion segundo driver
-	;mov bx, es:[2Ch]
-	
-	;mov ah, 49h
-	;int 21h
-	;mov es, bx
-	;int 21h
-	
-	;cli ;TODO
-	;mov ds:[1Ch*4], cx
-	;mov ds:[1Ch*4+2], cx
-	;sti	;TODO
-	
-	
-	jmp fininst
-	
-	nodesinst:
-	mov dx, OFFSET nodes
-	mov ah, 9
-	int 21H
-	jmp fininst
-	
-	
-	instrucciones:		;Imprimimos instrucciones si nos han dado algo no valido
+
+instrucciones PROC FAR
+	push ax dx es si
 	mov ah, 9
 	mov dx, OFFSET instruc	
 	int 21H
@@ -267,14 +158,157 @@ instalador PROC FAR
 	mov dx, OFFSET desinstal	;En caso contrario, notificamos que no esta instalado
 	mov ah, 9
 	int 21H
-	jmp fininst
-	instalado:	;Notificamos que esta instalado
+	jmp fininstrucciones
+instalado:
 	mov dx, OFFSET instal
 	mov ah, 9
+	int 21h
+fininstrucciones:
+	pop si es dx ax
+	ret
+instrucciones ENDP
+
+instalador PROC FAR
+	xor dh, dh
+	mov dl, cs:[args] ; Número de argumentos
+	cmp dl, 0
+	jnz cont1 ; Si no hay argumentos, mostramos unas instrucciones, pues no es una opcion valida
+	call instrucciones
+	jmp fininst
+cont1:
+	cmp dl, 3
+	jz instdes	;Si hay tres letras, PUEDE que haya puesto correctamente las letras
+	;aux:
+	;jmp instrucciones	;Si nos pasan algo no contemplado, imprimimos instrucciones
+	call instrucciones
+	jmp fininst
+instdes:		;Comprobamos si las tres letras son validas
+	mov dl, cs:[args+2]
+	cmp dl, '/'	;Si la el segundo caracter no es /, mostramos instrucciones
+	jz cont2
+	call instrucciones
+	jmp fininst
+cont2:
+	mov dl, cs:[args+3]
+	cmp dl, 'D'		;Si la tercera letra es D, desinstalamos
+	jz desinst
+	cmp dl, 'I'		;Si la tercera letra es I, instalamos
+	jz cont3			;Si no, imprimimos instrucciones
+	call instrucciones
+	jmp fininst
+	;inst:
+cont3:
+	mov cx, 0		
+	mov es, cx
+	mov cx, OFFSET rsi	;ax tiene el OFFSET del codigo Polibio
+	mov bx, cs
+	;cli					;Se inhabilita la interrupcion temporal
+	in ax, 21h
+	or ax, 1
+	out 21h, ax
+	
+	mov es:[57h*4], cx		;Ajustamos OFF Y SEG de la 57h
+	mov es:[57h*4+2], bx
+	
+	in ax, 21h
+	and ax, 1111111111111110b
+	out 21h, ax
+	;sti					;Rehabilitamos la interrupcion temporal
+	;mov dx, OFFSET instalador
+	mov cx, 0		
+	mov es, cx
+	
+	mov bx, es:[1Ch*4]	;Guardamos OFF Y SEG de la rutina de 1Ch anterior
+	mov cx, es:[1Ch*4+2]
+	mov off1C, bx
+	mov seg1C, cx
+	
+	mov cx, OFFSET rsi2	;ax tiene el OFFSET del codigo Polibio
+	mov bx, cs
+	;cli					
+	;Se inhabilita la interrupcion temporal
+	
+	in ax, 21h
+	or ax, 1
+	out 21h, ax
+	
+	mov es:[1Ch*4], cx		;Ajustamos OFF Y SEG de la 1Ch
+	mov es:[1Ch*4+2], bx
+	
+	in ax, 21h
+	and ax, 1111111111111110b
+	out 21h, ax
+	;sti					;Rehabilitamos la interrupcion temporal
+	mov dx, OFFSET instal
+	mov ah, 9
+	int 21h
+	
+	mov dx, OFFSET instalador
+	int 27h ; Acaba y deja residente 
+			; PSP, variables y rutina rsi.
+	
+desinst:
+	;Comprobamos si el driver que está instalado es el nuestro, si es que hay alguno
+	mov ax, 0				
+	mov es, ax				
+	cmp ax, es:[57h*4]	;Comprobamos si hay un driver instalado	
+	jz nodesinst		
+	mov ax, firma		;Firma de nuestro driver
+	mov si, es:[57h*4]
+	mov es, es:[57h*4+2]
+	cmp ax, es:[si-2]	;Comprobamos si la firma de nuestro driver es la que debe ser
+	jnz nodesinst	
+	mov bp, es:[si-6]
+	mov dx, es:[si-4]		;Obtenemos OFF Y SEG de la rsi original de 1Ch
+	
+	mov cx, 0				;Desinstalacion primer driver
+	mov ds, cx
+	mov es, ds:[57h*4+2]
+	mov bx, es:[2Ch]
+	
+	mov ah, 49h
+	int 21h
+	mov es, bx
+	int 21h
+	
+	cli
+	mov ds:[57h*4], cx
+	mov ds:[57h*4+2], cx
+	sti
+	
+	mov cx, 0
+	mov ds, cx
+	mov es, ds:[1Ch*4+2]	;Desisntalacion segundo driver
+	mov bx, es:[2Ch]
+	
+	mov ah, 49h
+	int 21h
+	mov es, bx
+	int 21h
+	
+	cli	;Recuperamos OFF Y SEG de la rsi original de 1Ch
+	mov ds:[1Ch*4], bp
+	mov ds:[1Ch*4+2], dx
+	sti	
+	
+	mov dx, cs
+	mov ds, dx
+	mov dx, OFFSET desinstal
+	mov ah, 9
+	int 21h
+	
+	jmp fininst
+	
+nodesinst:
+	mov dx, OFFSET nodes
+	mov ah, 9
 	int 21H
-	fininst:	;Fin del codigo
+	jmp fininst
+	
+fininst:	;Fin del codigo
 	mov ax, 4c00H
 	int 21H
 instalador ENDP
+
 codigo ENDS
 END inicio
--- p4/p4c.asm
+++ p4/p4c.asm
@@ -14,7 +14,7 @@ DATOS SEGMENT
 	INSTRUCCIONES6 DB 'IMPORTANTE: LA CADENA, TANTO EN COD COMO EN DECOD, TENDRA QUE ACABAR EN UN CARACTER DOLAR (SHIFT+4)',10,'$'
 	INPUT DB 'Introduzca el comando: $'
 	INPUTCOD DB 10, 'Introduzca la string a codificar (Solo mayusculas y numeros): $'
-	INPUTDECOD DB 10, 'Introduzca la string a decodificar (Solo numeros entre 1 y 6): $'
+	INPUTDECOD DB 10, 'Introduzca la string a decodificar (Solo numeros entre 1 y 6, tendran que ser un numero par de ellos): $'
 	ERRSTRING DB 10, 'Comando desconocido', 10, '$'
 	INTRO DB 10, '$'
 	STRING DB 100 dup(?)
@@ -38,143 +38,144 @@ EXTRA ENDS
 ; DEFINICION DEL SEGMENTO DE CODIGO
 CODE SEGMENT
 ASSUME CS: CODE, DS: DATOS, ES: EXTRA, SS: PILA
-; COMIENZO DEL PROCEDIMIENTO PRINCIPah
+; COMIENZO DEL PROCEDIMIENTO PRINCIPAL
 
 INICIO PROC
-mov ax, DATOS					;Movemos los segmentos a su sitio correspondiente
-mov ds, ax
-mov ax, PILA
-mov ss, ax
-mov ax, EXTRA
-mov es, ax
+	mov ax, DATOS					;Movemos los segmentos a su sitio correspondiente
+	mov ds, ax
+	mov ax, PILA
+	mov ss, ax
+	mov ax, EXTRA
+	mov es, ax
 initial:
-mov ah, 9
-mov dx, OFFSET INSTRUCCIONES	;Imprimimos las instrucciones
-int 21h
-mov dx, OFFSET INSTRUCCIONES2
-int 21h
-mov dx, OFFSET INSTRUCCIONES3
-int 21h
-mov dx, OFFSET INSTRUCCIONES4
-int 21h
-mov dx, OFFSET INSTRUCCIONES5
-int 21h
-mov dx, OFFSET INSTRUCCIONES6
-int 21h
-mov dx, OFFSET INPUT			;Solicitamos un comando
-int 21h
-mov ah,0AH						
-mov dx,OFFSET COMANDO			;Leemos el comando introducido
-mov COMANDO[0],10
-int 21H
-xor ax, ax						;Inicializamos ax a 0
-call CHECKCOM					;Checkeamos cual es el comando introducido
-cmp ax, 1						;Saltamos a codificar
-jz cod
-cmp ax, 2						;Saltamos a decodificar
-jz decod	
-cmp ax, 3						;No hacemos nada
-jz fin
-mov ah, 9
-mov dx, OFFSET ERRSTRING		;Error: no se ha hecho nada de lo que deberia haber hecho
-int 21h
-jmp initial						;En tal caso, volvemos al principio
-
+	mov ah, 9
+	mov dx, OFFSET INSTRUCCIONES	;Imprimimos las instrucciones
+	int 21h
+	mov dx, OFFSET INSTRUCCIONES2
+	int 21h
+	mov dx, OFFSET INSTRUCCIONES3
+	int 21h
+	mov dx, OFFSET INSTRUCCIONES4
+	int 21h
+	mov dx, OFFSET INSTRUCCIONES5
+	int 21h
+	mov dx, OFFSET INSTRUCCIONES6
+	int 21h
+	mov dx, OFFSET INPUT			;Solicitamos un comando
+	int 21h
+	mov ah,0AH						
+	mov dx,OFFSET COMANDO			;Leemos el comando introducido
+	mov COMANDO[0],10
+	int 21H
+	xor ax, ax						;Inicializamos ax a 0
+	call CHECKCOM					;Checkeamos cual es el comando introducido
+	cmp ax, 1						;Saltamos a codificar, ya que 1 representa cod
+	jz cod
+	cmp ax, 2						;Saltamos a decodificar, ya que 2 representa decod
+	jz decod	
+	cmp ax, 3						;Saltamos a quit, ya que 3 representa quit
+	jz fin
+	mov ah, 9
+	mov dx, OFFSET ERRSTRING		;Error: el comando introducido no es ninguno de los soportados
+	int 21h
+	jmp initial						;En tal caso, volvemos al principio
+	
 cod:							;Procedemos a la codificacion
-mov ah, 9h
-mov dx, OFFSET INPUTCOD			;Solicitamos una string
-int 21h
-mov ah, 0AH
-mov dx,OFFSET STRING			;Leemos la string
-mov STRING[0],99
-int 21H
-mov ah, 9
-mov dx, OFFSET INTRO			;Imprimimos un salto de linea
-int 21H
-mov ah, 10H
-mov dx, OFFSET STRING[2]
-int 57H							;Llamamos al driver para imprimir la string codificada
-mov ah, 9
-mov dx, OFFSET INTRO			;Imprimimos un salto de linea
-int 21H
-jmp initial						;Volvemos al principio
-
+	mov ah, 9h
+	mov dx, OFFSET INPUTCOD			;Solicitamos una string
+	int 21h
+	mov ah, 0AH
+	mov dx,OFFSET STRING			;Leemos la string
+	mov STRING[0],99
+	int 21H
+	mov ah, 9
+	mov dx, OFFSET INTRO			;Imprimimos un salto de linea
+	int 21H
+	mov ah, 10H
+	mov dx, OFFSET STRING[2]
+	int 57H							;Llamamos al driver para imprimir la string codificada
+	mov ah, 9
+	mov dx, OFFSET INTRO			;Imprimimos un salto de linea
+	int 21H
+	jmp initial						;Volvemos al principio
+	
 decod:							;Procedemos a la decodificacion
-mov ah, 9h
-mov dx, OFFSET INPUTDECOD		;Solicitamos una string a decodificar
-int 21h
-mov ah, 0AH
-mov dx,OFFSET STRING			;Leemos la string que nos han dado
-mov STRING[0],99
-int 21H
-mov ah, 9
-mov dx, OFFSET INTRO			;Imprimimos un salto de linea
-int 21H
-mov ah, 11H
-mov dx, OFFSET STRING[2]
-int 57H							;Llamamos al driver para imprimir la string decodificada
-mov ah, 9
-mov dx, OFFSET INTRO			;Imprimimos un salto de linea
-int 21H
-jmp initial						;Volvemos al inicio
+	mov ah, 9h
+	mov dx, OFFSET INPUTDECOD		;Solicitamos una string a decodificar
+	int 21h
+	mov ah, 0AH
+	mov dx,OFFSET STRING			;Leemos la string que nos han dado
+	mov STRING[0],99
+	int 21H
+	mov ah, 9
+	mov dx, OFFSET INTRO			;Imprimimos un salto de linea
+	int 21H
+	mov ah, 11H
+	mov dx, OFFSET STRING[2]
+	int 57H							;Llamamos al driver para imprimir la string decodificada
+	mov ah, 9
+	mov dx, OFFSET INTRO			;Imprimimos un salto de linea
+	int 21H
+	jmp initial						;Volvemos al inicio
 fin:
-mov ax, 4c00h					;Fin del programa
-int 21h
+	mov ax, 4c00h					;Fin del programa
+	int 21h
 INICIO ENDP
 
 CHECKCOM PROC		;DEVUELVE EN AX 1 SI EL COMANDO ES COD, 2 SI ES DECOD, 3 SI ES QUIT. SI NO, LO DEJA COMO ESTABA (A 0)
-push bx
-mov bx, 2				;Valor en el que comenzamos a ver el comando
+	push bx
+	mov bx, 2				;bx ira recorriendo los caracteres utiles de COMANDO, omitiendo los 2 primeros bytes
+							;bx tambien nos sirve para ir recorriendo la string a comparar de memoria con 2 bytes menos
 buclecod:				
-mov ah, COMANDO[bx]		;Valor del comando
-cmp ah, 0DH				
-jz checkcod				;Comprobamos que si se esta solicitando la codificacion
-cmp COD[bx-2], ah		
-jnz initdecod
-inc bx
-jmp buclecod
+	mov ah, COMANDO[bx]		
+	cmp ah, 0DH				;Si es retorno de carro, ya ha terminado la string introducida
+	jz checkcod				;Saltamos a comprobar si la otra string tambien ha terminado
+	cmp COD[bx-2], ah		;Si no, comparemos los caracteres de las dos cadenas
+	jnz initdecod			;Si no son iguales, no es el comando cod
+	inc bx					
+	jmp buclecod			;Si son iguales seguimos comprobando hasta llegar al retorno de carro
 checkcod:
-cmp COD[bx-2], '$'
-jz rescod
+	cmp COD[bx-2], '$'	
+	jz rescod				;Si la string con la que comparamos también ha terminado, marcaremos que el comando es cod
+							;Si no, comparamos COMANDO con otro posible comando
 initdecod:
-mov bx, 2
+	mov bx, 2				;Inicializamos el índice
 bucledecod:
-mov ah, COMANDO[bx]
-cmp ah, 0DH
-jz checkdecod
-cmp DECOD[bx-2], ah
-jnz initquit
-inc bx
-jmp bucledecod
+	mov ah, COMANDO[bx]
+	cmp ah, 0DH				
+	jz checkdecod			;Si es retorno de carro, hay que comprobar que la otra string tambien ha acabado
+	cmp DECOD[bx-2], ah		;Si no, comparamos los caracteres de las dos cadenas
+	jnz initquit			;Si no son iguales, no es el comando decod
+	inc bx
+	jmp bucledecod			;Si son iguales seguimos comprobando
 checkdecod:
-cmp DECOD[bx-2], '$'
-jz resdecod
+	cmp DECOD[bx-2], '$'	
+	jz resdecod				;Si la string con la que comparamos tambien ha terminado, marcaremos que el comando es decod
 initquit:
-mov bx, 2
+	mov bx, 2				;Si no, comparamos COMANDO con otro posible comando
 buclequit:
-mov ah, COMANDO[bx]
-cmp ah, 0DH
-jz checkquit
-cmp QUIT[bx-2], ah
-jnz fincheck
-inc bx
-jmp buclequit
+	mov ah, COMANDO[bx]
+	cmp ah, 0DH				
+	jz checkquit			;Si es retorno de carro, hay que comprobar que la otra string tambien ha acabado
+	cmp QUIT[bx-2], ah		;Si no, comparamos los caracteres de las dos cadenas
+	jnz fincheck			;Si no son iguales, no es el comando quit
+	inc bx
+	jmp buclequit			;Si son iguales seguimos comprobando
 checkquit:
-cmp QUIT[bx-2], '$'
-jz resquit
-jmp fincheck
-jmp buclequit
+	cmp QUIT[bx-2], '$'	
+	jz resquit				;Si la otra string tambien ha terminado, marcaremos que el comando es quit
+	jmp fincheck
 rescod:
-mov ax, 1
-jmp fincheck
+	mov ax, 1				;Ponemos ax a 1 indicando que el comando es cod
+	jmp fincheck
 resdecod:
-mov ax, 2
-jmp fincheck
+	mov ax, 2				;Ponemos ax a 2 indicando que el comando es decod
+	jmp fincheck
 resquit:
-mov ax, 3
+	mov ax, 3				;Ponemos ax a 3 indicando que el comando es quit
 fincheck:
-pop bx
-ret
+	pop bx
+	ret
 CHECKCOM ENDP
 CODE ENDS
 END INICIO
